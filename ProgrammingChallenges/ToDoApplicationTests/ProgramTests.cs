a1478e02187d0dcac917d28596242e135488d54d
BotMaster3000 <jdgebauer@gmx.de>
2018-12-10 19:06:29 +0100

    Added Unit-Test-Project to ToDoApplication

--- ProgrammingChallenges/ToDoApplicationTests/ProgramTests.cs
+++ ProgrammingChallenges/ToDoApplicationTests/ProgramTests.cs
@@ -4,12 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ToDoApplication
+namespace ToDoApplicationTests
 {
-    public class Program
+    class ProgramTests
     {
-        public static void Main(string[] args)
-        {
-        }
     }
 }
