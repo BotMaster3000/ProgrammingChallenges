d694189d69fe84a1557498d2c5fb6d5cdb1ee3c1
BotMaster3000 <jdgebauer@gmx.de>
2018-12-10 20:15:49 +0100

    Added IChainSentenceGenerator and Implementation

---
+++ ProgrammingChallenges/MarkovChainSentenceGenerator/Interfaces/IChainSentenceGenerator.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarkovChainSentenceGenerator.Interfaces
+{
+    public interface IChainSentenceGenerator
+    {
+        IWordModel[] WordModels { get; set; }
+        string GenerateSentence();
+    }
+}
---
+++ ProgrammingChallenges/MarkovChainSentenceGenerator/Logic/ChainSentenceGenerator.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MarkovChainSentenceGenerator.Interfaces;
+
+namespace MarkovChainSentenceGenerator.Logic
+{
+    public class ChainSentenceGenerator : IChainSentenceGenerator
+    {
+        public IWordModel[] WordModels { get; set; }
+
+        public string GenerateSentence()
+        {
+            return "";
+        }
+    }
+}
