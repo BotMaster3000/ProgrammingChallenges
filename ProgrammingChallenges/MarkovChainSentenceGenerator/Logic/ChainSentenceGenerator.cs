c5c6c430e021f0e4498654dce4052d6e212905a1
BotMaster3000 <jdgebauer@gmx.de>
2018-12-10 20:44:26 +0100

    Implemented Logic for the ChainSentenceGenerator and Implemented the UnitTest

--- ProgrammingChallenges/MarkovChainSentenceGenerator/Logic/ChainSentenceGenerator.cs
+++ ProgrammingChallenges/MarkovChainSentenceGenerator/Logic/ChainSentenceGenerator.cs
@@ -15,7 +15,55 @@ namespace MarkovChainSentenceGenerator.Logic
 
         public string GenerateSentence()
         {
+            IWordModel currentWord = GetRandomWord();
+            string returnSentence = currentWord.Word;
 
+            for (int i = 0; i < 10; ++i)
+            {
+                string[] wordPool = null;
+                wordPool = GenerateWordPool(currentWord);
+                while (wordPool?.Length == 0)
+                {
+                    currentWord = GetRandomWord();
+                    wordPool = GenerateWordPool(currentWord);
+                }
+
+                string chosenWord = wordPool[rand.Next(0, wordPool.Length)];
+                returnSentence += " " + chosenWord;
+                currentWord = GetNextWordModel(chosenWord);
+            }
+
+            return returnSentence;
+        }
+
+        private IWordModel GetRandomWord()
+        {
+            return WordModels[rand.Next(0, WordModels.Length - 1)];
+        }
+
+        private string[] GenerateWordPool(IWordModel word)
+        {
+            List<string> wordPool = new List<string>();
+            foreach (KeyValuePair<string, int> wordAndOccurance in word.WordAndOccurance)
+            {
+                for (int i = 0; i < wordAndOccurance.Value; ++i)
+                {
+                    wordPool.Add(wordAndOccurance.Key);
+                }
+            }
+            return wordPool.ToArray();
+        }
+
+        private IWordModel GetNextWordModel(string word)
+        {
+            foreach (IWordModel wordmodel in WordModels)
+            {
+                if (wordmodel.Word == word)
+                {
+                    return wordmodel;
+                }
+            }
+            return null;
         }
     }
 }
--- ProgrammingChallenges/MarkovChainSentenceGeneratorTests/Logic/ChainSentenceGeneratorTests.cs
+++ ProgrammingChallenges/MarkovChainSentenceGeneratorTests/Logic/ChainSentenceGeneratorTests.cs
@@ -5,16 +5,48 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace MarkovChainSentenceGenerator.Logic.Tests
 {
     [TestClass]
     public class ChainSentenceGeneratorTests
     {
+        private const string testFileName = "Test.txt";
+        private const string testFileContent = "Test, this is a test, a test for testing the tests created for testing apple pies and lemon cakes whereas apple cakes and pies come together";
+
+        private void GenerateTestFile()
+        {
+            File.Create(testFileName).Close();
+            File.AppendAllText(testFileName, testFileContent);
+        }
+
         [TestMethod]
         public void GenerateSentenceTest()
         {
+            GenerateTestFile();
 
+            ChainSentenceGenerator sentenceGenerator = new ChainSentenceGenerator();
+            sentenceGenerator.WordModels = new DataCollector().ParseDataFromFile(testFileName);
+
+            string result = sentenceGenerator.GenerateSentence();
+
+            foreach (string resultWord in result.Split(' '))
+            {
+                bool wordExistsInOriginalSentence = false;
+                foreach (string word in testFileContent.Split(' '))
+                {
+                    if(resultWord == word)
+                    {
+                        wordExistsInOriginalSentence = true;
+                        break;
+                    }
+                }
+                if (!wordExistsInOriginalSentence)
+                {
+                    Assert.Fail("Word does not exist in original sentence: " + resultWord);
+                }
+            }
         }
     }
 }
