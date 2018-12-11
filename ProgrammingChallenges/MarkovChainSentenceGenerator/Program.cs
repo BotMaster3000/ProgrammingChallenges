a2c803e6c5580c04682685728a04a49ccb2228b6
BotMaster3000 <jdgebauer@gmx.de>
2018-12-11 20:06:33 +0100

    Added Logic to Program.cs to give an example-use of the Project

--- ProgrammingChallenges/MarkovChainSentenceGenerator/Program.cs
+++ ProgrammingChallenges/MarkovChainSentenceGenerator/Program.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
+using MarkovChainSentenceGenerator.Logic;
+using MarkovChainSentenceGenerator.Interfaces;
 
 namespace MarkovChainSentenceGenerator
 {
@@ -10,6 +13,28 @@ namespace MarkovChainSentenceGenerator
     {
         public static void Main(string[] args)
         {
+            string fileName = "";
+            do
+            {
+                Console.WriteLine("Write a file you want to read from:");
+                fileName = Console.ReadLine();
+            }
+            while (!File.Exists(fileName));
+
+            Console.WriteLine("Parsing File. Please wait.");
+            DataCollector collector = new DataCollector();
+            IWordModel[] wordModelArray = collector.ParseDataFromFile(fileName);
+
+            Console.WriteLine("Generating Sentences. Type /s to abort and exit");
+            ChainSentenceGenerator sentenceGenerator = new ChainSentenceGenerator()
+            {
+                WordModels = wordModelArray
+            };
+            do
+            {
+                Console.WriteLine(sentenceGenerator.GenerateSentence());
+            }
+            while (Console.ReadLine() != "/s");
         }
     }
 }
