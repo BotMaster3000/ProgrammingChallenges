e54576d6a2e86a92aedcc3bd04c08bca63d5ac09
BotMaster3000 <jdgebauer@gmx.de>
2018-12-11 20:30:54 +0100

    Added Interfaces IEntryModel and IToDoListHandler

---
+++ ProgrammingChallenges/ToDoApplication/Interfaces/IToDoListHandler.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToDoApplication.Interfaces
+{
+    public interface IToDoListHandler
+    {
+        List<IEntryModel> EntryModels { get; set; }
+
+        void LoadEntries();
+        void DisplayEntries();
+        void DisplayOldEntries();
+        void AddEntry();
+        void RemoveEntry();
+    }
+}
